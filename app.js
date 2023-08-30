3a0b3cf5d24f304d26ef47482fc9fad5acf8d1a0
ANGEL TURKS <angelturks17@gmail.com>
2023-08-30 09:04:57 -0400

    changed js file

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ function CookieSales(name, minCust, maxCust, avgCookieSale) {
 }
 
 CookieSales.prototype.getRandomCust = function () {
-  return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
+return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
 };
 
 CookieSales.prototype.calculateCookiePerHour = function () {
-  for (let hour = 6; hour <= 19; hour++) {
+  for (let hour = 6; hour <= 20; hour++) {
     const cust = this.getRandomCust();
     const cookieSold = Math.round(cust * this.avgCookieSale);
     this.cookiePerHour.push(cookieSold);
@@ -26,38 +26,31 @@ CookieSales.prototype.displaySales = function () {
   const salesList = document.createElement('ul');
   salesList.innerHTML = `<strong>${this.name}</strong>`;
   let totalCookies = 0;
-  for (let hour = 6; hour <= 19; hour++) {
+  for (let hour = 6; hour <= 20; hour++) {
     const listItems = document.createElement('li');
-    listItems.textContent = `${hour}am: ${this.cookiePerHour[hour - 6]} cookies`;
+    let displayHour;
+
+    if (hour <= 12) {
+      displayHour = `${hour}am`;
+    } else if (hour === 12) {
+      displayHour = `${hour}pm`;
+    } else {
+      displayHour = `${hour - 12}pm`;
+    }
+
+    listItems.textContent = `${displayHour}: ${this.cookiePerHour[hour - 6]} cookies`;
     salesList.appendChild(listItems);
     totalCookies += this.cookiePerHour[hour - 6];
   }
-
+  
   const totalItems = document.createElement('li');
-  totalItems.textContent = `Total: ${totalCookies} cookies`;
+  totalItems.innerHTML = `<strong>Total: ${totalCookies} cookies</strong>`;
   salesList.appendChild(totalItems);
 
-  hourSection.appendChild(salesList); 
+hourSection.appendChild(salesList);
 };
 
 const seattle = new CookieSales('seattle', 23, 65, 6.3);
 
-const tokyo = new CookieSales('tokyo', 3, 24, 1.2);
-const dubai = new CookieSales('dubai', 11, 38, 3.7);
-const paris = new CookieSales('paris', 20, 38, 2.3);
-const lima = new CookieSales('lima', 2, 16, 4.6);
-
 seattle.calculateCookiePerHour();
 seattle.displaySales();
-
-tokyo.calculateCookiePerHour();
-tokyo.displaySales();
-
-dubai.calculateCookiePerHour();
-dubai.displaySales();
-
-paris.calculateCookiePerHour();
-paris.displaySales();
-
-lima.calculateCookiePerHour();
-lima.displaySales();
